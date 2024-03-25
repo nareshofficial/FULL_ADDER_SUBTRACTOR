568986afebe043bd471fc3ab8814f81a5d2aac4e
aaron-h-2k5 <144250957+aaron-h-2k5@users.noreply.github.com>
2024-03-25 08:58:59 +0530

    Update FULL_addsub.v

--- FULL_addsub.v
+++ FULL_addsub.v
@@ -1,8 +1,11 @@
 module FULL_addsub(a,b,cin,sum,carry,BO,DIFF);
 input a,b,cin;
 output sum,carry,BO,DIFF;
-//Write syntax for full adder sum and carry in date flow modelling 
+assign sum = a^b^cin;
+assign carry = (a&b) | (b&cin) | (a&cin);
 wire a0;
 not (a0,a);
-//Write syntax for full subtractor Borrow and Difference in date flow modelling
+//Write syntax for full subtractor Borrow and Difference in data flow modelling
+assign DIFF = a^b^cin;
+assign BO = (a0&b) | (b&cin) | (a0&cin);
 endmodule
